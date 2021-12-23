5adc7d1ffd6956dd6e5207be4adc3d197ea2475e
danted9822 <halaszpeter9814@gmail.com>
2021-12-23 15:10:54 +0100

    Add items to the basket , View them in the cart and adding loading screen

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -7,6 +7,9 @@ const sleep = () => new Promise(resolve => setTimeout(resolve, 500))
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 
+//the browser wiil recive a cookie and it will set the cookie inside my appliction storage
+axios.defaults.withCredentials = true;
+
 const responseBody = (response: AxiosResponse ) => response.data;
 
 axios.interceptors.response.use(async response => {
@@ -68,9 +71,16 @@ const TestErrors = {
     getValidationError : () => requests.get('buggy/validation-error'),
 }
 
+const Basket = {
+    get: () => requests.get('basket'),
+    addItem: (productId: number, quantity = 1) => requests.post(`basket?productId=${productId}&quantity=${quantity}`,{}),
+    removeItem: (productId: number, quantity = 1) => requests.delete(`basket?productId=${productId}&quantity=${quantity}`)
+}
+
 const agent = {
     Catalog,
-    TestErrors
+    TestErrors,
+    Basket
 
 }
 
