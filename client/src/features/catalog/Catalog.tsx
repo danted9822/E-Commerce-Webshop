252aa91cd1fe64361c25161ee12f326ebab84171
danted9822 <halaszpeter9814@gmail.com>
2022-03-24 11:57:20 +0100

    Sorting by name or price

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,8 +1,9 @@
-import { Box, Checkbox, FormControl, FormControlLabel, FormGroup, Grid, Pagination, Paper, Radio, RadioGroup,Typography } from "@mui/material";
+import { Box, Checkbox, FormControlLabel, FormGroup, Grid, Pagination, Paper, Typography } from "@mui/material";
 import { useEffect } from "react";
+import RadioButtonGroup from "../../app/components/RadioButtonGroup";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/ConfigureStore";
-import { fetchFilters, fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors, setProductParams } from "./catalogSlice";
 import ProductList from "./ProductList";
 import ProdcuctSearch from "./ProductSearch";
 
@@ -17,7 +18,7 @@ const sortOptions = [
 
 export default function Catalog(){
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded, status, filtersLoaded,brands,types} = useAppSelector(state => state.catalog);
+    const {productsLoaded, status, filtersLoaded,brands,types,productParams} = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
     
 
@@ -39,14 +40,14 @@ export default function Catalog(){
                 <ProdcuctSearch/>
                 </Paper>
                 <Paper sx ={{mb:2,p:2}}>
-                    <FormControl>
-                        <RadioGroup>
-                         {sortOptions.map(({value, label }) => (
-                            <FormControlLabel value={value} control={<Radio />} label={label} key={value} />
-                            ))}
-
-                        </RadioGroup>
-                    </FormControl>
+                <RadioButtonGroup
+                    selectedValue={productParams.orderBy}
+                    options = {sortOptions}
+                    onChange = {(e) => dispatch(setProductParams({orderBy: e.target.value}))}
+                
+                />
+                    
+                
                 </Paper>
 
                 <Paper sx={{mb:2,p:2}}>
